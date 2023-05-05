6a96ccbc7d6b4c00aace1dd7158ab7f1385fa310
fdgardon <119641606+fdgardon@users.noreply.github.com>
2023-05-04 22:06:35 -0700

    Thought model is updated and fixed

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+// Schema to create a course model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlenght: 280,
+      minlenght: 1,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      //Use a getter method to format the timestamp on query
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    
+    reactions: [reactionSchema]
+   
+    
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Course = model('Thought', thoughtSchema);
+
+module.exports = Thought;
